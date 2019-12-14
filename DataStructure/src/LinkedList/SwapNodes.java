c92160ba8c41c0ee481d38f10e122aa52a278e9c
Rahul Tripathi <Rahul Tripathi@192.168.0.2>
2019-12-14 09:16:21 +0530

    Added program for swapping the nodes in LinkedList

---
+++ DataStructure/src/LinkedList/SwapNodes.java
@@ -0,0 +1,64 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class SwapNodes {
+
+	public static void main(String[] args) {
+		Node head = initialiseLinkedList();
+
+		System.out.println("Current list is:-> ");
+		printList(head);
+
+		head = swapNode(head);
+
+		System.out.println("List after remove duplicate item is:-> ");
+		printList(head);
+	}
+
+	private static Node swapNode(Node head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		Node dummy = new Node(0);
+		dummy.next = head;
+		Node ptr = dummy;
+
+		while (ptr.next != null && ptr.next.next != null) {
+			Node temp1 = ptr;
+			ptr = ptr.next;
+			temp1.next = ptr.next;
+
+			Node temp2 = ptr.next.next;
+			ptr.next.next = ptr;
+			ptr.next = temp2;
+		}
+		return dummy.next;
+	}
+
+	private static void printList(Node head) {
+		Node curr = head;
+		while (curr != null) {
+			System.out.print(curr.item + " ");
+			curr = curr.next;
+		}
+		System.out.println();
+	}
+
+	private static Node initialiseLinkedList() {
+		System.out.println("Enter number count for LinkedList:-> ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Enter numbers:-> ");
+		int num = sc.nextInt();
+		Node head = new Node(num);
+		Node curr = head;
+		for (int i = 0; i < n - 1; i++) {
+			num = sc.nextInt();
+			head.next = new Node(num);
+			head = head.next;
+		}
+		return curr;
+	}
+}
