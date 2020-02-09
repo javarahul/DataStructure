537d4387e9b7ec071544ab9b15bb62d20d28b509
Rahul Tripathi <Rahul Tripathi@192.168.0.8>
2020-02-09 20:52:15 +0530

    Tree Node creation class

---
+++ DataStructure/src/tree/Node.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class Node {
+	public int val;
+	public Node left;
+	public Node right;
+
+	public Node(int val) {
+		this.val = val;
+		left = null;
+		right = null;
+	}
+}
