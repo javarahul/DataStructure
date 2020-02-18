6179cbf8bb58c9c33eeeae522f2cf11027f3c52a
Rahul Tripathi <Rahul Tripathi@192.168.0.3>
2020-02-18 05:49:16 +0530

    Program for Level Order Traversal

---
+++ DataStructure/src/tree/LevelOrderTraversal1.java
@@ -0,0 +1,59 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LevelOrderTraversal1 {
+
+	public static void main(String[] args) {
+		Node root = initializeTree();
+
+		ArrayList<ArrayList<Integer>> result = levelOrderTraversal(root);
+
+		System.out.println("List after In order traversal:-> " + result);
+
+	}
+
+	private static ArrayList<ArrayList<Integer>> levelOrderTraversal(Node root) {
+		ArrayList<ArrayList<Integer>> al = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> nodeValues = new ArrayList<Integer>();
+		if (root == null)
+			return al;
+
+		LinkedList<Node> current = new LinkedList<Node>();
+		LinkedList<Node> next = new LinkedList<Node>();
+		current.add(root);
+
+		while (!current.isEmpty()) {
+			Node node = current.remove();
+
+			if (node.left != null)
+				next.add(node.left);
+			if (node.right != null)
+				next.add(node.right);
+
+			nodeValues.add(node.val);
+			if (current.isEmpty()) {
+				current = next;
+				next = new LinkedList<Node>();
+				al.add(nodeValues);
+				nodeValues = new ArrayList<>();
+			}
+
+		}
+		return al;
+	}
+
+	private static Node initializeTree() {
+		Node root = new Node(10);
+		root.left = new Node(20);
+		root.right = new Node(30);
+		root.left.left = new Node(40);
+		root.left.left.left = new Node(70);
+		root.left.right = new Node(50);
+		root.right.left = new Node(60);
+		root.left.left.right = new Node(80);
+
+		return root;
+	}
+}
