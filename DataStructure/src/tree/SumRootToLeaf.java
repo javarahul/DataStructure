fc60731186548060881193d881c61cb8d0174e17
Rahul Tripathi <Rahul Tripathi@RahulTripathi>
2020-09-28 16:57:10 +0530

    Updated the test cases from GeeksForGeeks

--- DataStructure/src/tree/SumRootToLeaf.java
+++ DataStructure/src/tree/SumRootToLeaf.java
@@ -33,14 +33,14 @@ public class SumRootToLeaf {
 	}
 
 	private static Node initializeTree() {
-		Node root = new Node(10);
-		root.left = new Node(20);
-		root.right = new Node(30);
-		root.left.left = new Node(40);
-		root.left.left.left = new Node(70);
-		root.left.right = new Node(50);
-		root.right.left = new Node(60);
-		root.left.left.right = new Node(80);
+		Node root = new Node(6);
+		root.left = new Node(3);
+		root.right = new Node(5);
+		root.right.right = new Node(4);
+		root.left.left = new Node(2);
+		root.left.right = new Node(5);
+		root.left.right.right = new Node(4);
+		root.left.right.left = new Node(7);
 
 		return root;
 	}
