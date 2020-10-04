64cca9af5788fa15ad1d90f19ed13b40642ccf09
Rahul Tripathi <Rahul Tripathi@RahulTripathi>
2020-10-04 14:27:38 +0530

    program for getting the MaxPath of a tree

---
+++ DataStructure/src/tree/MaxDepth1.java
@@ -0,0 +1,58 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MaxDepth1 {
+
+	public static void main(String[] args) {
+		Node root = initializeTree();
+
+		System.out.println("Inorder Traversal of the BST:-> ");
+		Node tempRoot = root;
+		printInorder(tempRoot);
+		System.out.println();
+
+		int maxDepth = getMaxDepth(root);
+
+		System.out.println(maxDepth);
+	}
+
+	private static int getMaxDepth(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		int lDepth = getMaxDepth(root.left);
+		int rDepth = getMaxDepth(root.right);
+
+		if (lDepth > rDepth) {
+			return lDepth + 1;
+		} else {
+			return rDepth + 1;
+		}
+	}
+
+	private static Node initializeTree() {
+		Node root = new Node(9);
+		root.left = new Node(4);
+		root.right = new Node(17);
+		root.left.left = new Node(3);
+		root.left.right = new Node(6);
+		root.left.right.left = new Node(5);
+		root.left.right.right = new Node(7);
+		root.right.right = new Node(22);
+		root.right.right.left = new Node(20);
+
+		return root;
+	}
+
+	private static void printInorder(Node root) {
+		if (root == null) {
+			return;
+		}
+		printInorder(root.left);
+		System.out.print(root.val + " ");
+		printInorder(root.right);
+	}
+}
