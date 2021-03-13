da7987edf69f6cf01701904e8aed16c5ae1808d7
Rahul Tripathi <Rahul Tripathi@RahulTripathi>
2021-03-13 15:07:30 +0530

    Program for Bubble Sort

---
+++ DataStructure/src/sorting/BubbleSort1.java
@@ -0,0 +1,39 @@
+package sorting;
+
+public class BubbleSort1 {
+
+	public static void main(String[] args) {
+		int[] arr = { 9, 2, 4, 7, 3, 7, 10 };
+
+		System.out.println("Before Sorting");
+		print(arr);
+
+		bubbleSort(arr);
+
+		System.out.println();
+		System.out.println("After Sorting");
+		print(arr);
+
+	}
+
+	private static void bubbleSort(int[] arr) {
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	private static void print(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
