a3747d19d6f92652873197e6fac013de06babf9a
javarahul <tripathi.rahul15@gmail.com>
2022-01-25 18:58:03 +0530

    Commented dist array print statements

--- DataStructure/src/hashing/UnionForUnsortedArrayNaive.java
+++ DataStructure/src/hashing/UnionForUnsortedArrayNaive.java
@@ -52,9 +52,9 @@ public class UnionForUnsortedArrayNaive {
 			}
 		}
 
-		for (int i = 0; i < dist.length; i++) {
-			System.out.print(dist[i] + ", ");
-		}
+		/*
+		 * for (int i = 0; i < dist.length; i++) { System.out.print(dist[i] + ", "); }
+		 */
 		return result;
 	}
 }
