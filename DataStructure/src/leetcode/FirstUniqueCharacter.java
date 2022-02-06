627632c33efec2d6024a987cbf6f2a12e8eff56b
javarahul <tripathi.rahul15@gmail.com>
2022-02-06 13:53:56 +0530

    Update FirstUniqueCharacter.java

--- DataStructure/src/leetcode/FirstUniqueCharacter.java
+++ DataStructure/src/leetcode/FirstUniqueCharacter.java
@@ -10,16 +10,17 @@ public class FirstUniqueCharacter {
 
 	private static int firstUniqueChar(String s) {
 		for (int i = 0; i < s.length(); i++){
-            boolean flag = false;
-            for (int j = 0; j < s.length(); j++){
-                if (i != j && s.charAt(i) == s.charAt(j)){
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag){
-                return i;
-            }
-        }
-        return -1;	
+            		boolean flag = false;
+            		for (int j = 0; j < s.length(); j++){
+                		if (i != j && s.charAt(i) == s.charAt(j)){
+                    			flag = true;
+                    			break;
+                		}
+            		}
+            		if (!flag){
+                		return i;
+           		}
+        	}
+       		return -1;
+	}
 }
