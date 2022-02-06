4ef5c8ed98e19f2be6f8c5af98ed2eeffa17c866
javarahul <tripathi.rahul15@gmail.com>
2022-02-06 13:55:55 +0530

    Create FirstUniqueCharacter.java
    Program for finding the index for first unique character

---
+++ DataStructure/src/leetcode/string/FirstUniqueCharacter.java
@@ -0,0 +1,26 @@
+package leatcode.string;
+
+public class FirstUniqueCharacter {
+
+	public static void main(String[] args) {
+		String s = "leetcode";
+		int index = firstUniqueChar(s);
+		System.out.println("Index of first unique chararcter:-> " + index);
+	}
+
+	private static int firstUniqueChar(String s) {
+		for (int i = 0; i < s.length(); i++){
+            		boolean flag = false;
+            		for (int j = 0; j < s.length(); j++){
+                		if (i != j && s.charAt(i) == s.charAt(j)){
+                    			flag = true;
+                    			break;
+                		}
+            		}
+            		if (!flag){
+                		return i;
+           		}
+        	}
+       		return -1;
+	}
+}
