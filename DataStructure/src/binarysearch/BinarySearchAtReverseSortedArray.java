f322538cf285590b25487641abc1e74929245692
javarahul <tripathi.rahul15@gmail.com>
2022-04-03 20:40:34 +0530

    Create BinarySearchAtReverseSortedArray.java

---
+++ DataStructure/src/binarysearch/BinarySearchAtReverseSortedArray.java
@@ -0,0 +1,37 @@
+package binarysearch;
+
+import java.util.Scanner;
+
+public class BinarySearchAtReverseSortedArray {
+
+	public static void main(String[] args) {
+		int array[] = { 20, 19, 17, 14, 12, 11, 10, 8, 5, 3 };
+		System.out.println("Eanter a number which you want to search:-> ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		int index = binarySearch(array, num);
+		if (index > -1) {
+			System.out.println(num + " present in index:-> " + index);
+		} else {
+			System.out.println(num + " not present");
+		}
+	}
+
+	private static int binarySearch(int[] array, int num) {
+		int start = 0;
+		int end = array.length - 1;
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (num < array[mid]) {
+				start = mid + 1;
+			} else if (num > array[mid]) {
+				end = mid - 1;
+			} else if (num == array[mid]) {
+				return mid;
+			}
+		}
+		return -1;
+	}
+}
