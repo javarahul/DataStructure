0ca89282b6a7ed22f82dc8214091c2e92bfbe80a
javarahul <tripathi.rahul15@gmail.com>
2022-05-06 07:14:50 +0530

    Create FrequencySortIncreasingOrder.java

---
+++ DataStructure/src/heap/FrequencySortIncreasingOrder.java
@@ -0,0 +1,46 @@
+package heap;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+
+public class FrequencySortIncreasingOrder {
+
+	public static void main(String[] args) {
+		Integer[] arr = { -1, 1, -6, 4, 5, -6, 1, 4, 1 };
+		sort(arr);
+	}
+
+	public static void sort(Integer arr[]) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < arr.length; i++) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+		}
+
+		PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(new Comparator<>() {
+
+			@Override
+			public int compare(Entry<Integer, Integer> entry1, Entry<Integer, Integer> entry2) {
+				return entry1.getValue().compareTo(entry2.getValue());
+			}
+		});
+
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			minHeap.add(entry);
+		}
+
+		while (minHeap.size() > 0) {
+			int freq = minHeap.peek().getValue();
+			int item = minHeap.peek().getKey();
+
+			for (int i = 0; i < freq; i++) {
+				System.out.print(item + " ");
+			}
+
+			minHeap.poll();
+		}
+	}
+}
