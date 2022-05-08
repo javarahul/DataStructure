b95771d0adbc5d7bec9a4fce719e082c681c8f41
javarahul <tripathi.rahul15@gmail.com>
2022-05-08 17:18:44 +0530

    Create NextGreaterToRightElement.java

---
+++ DataStructure/src/stack/NextGreaterToRightElement.java
@@ -0,0 +1,24 @@
+package stack;
+
+public class NextGreaterToRightElement {
+
+	public static void main(String[] args) {
+		int arr[] = { 11, 13, 21, 3 };
+		getNextGreaterToRight(arr);
+	}
+
+	public static void getNextGreaterToRight(int[] arr) {
+		int nextGreater = -1;
+
+		for (int i = 0; i < arr.length; i++) {
+			nextGreater = -1;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					nextGreater = arr[j];
+					break;
+				}
+			}
+			System.out.println(arr[i] + " -- " + nextGreater);
+		}
+	}
+}
