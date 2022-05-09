a1424b496a9b2cc29943fe658011e2ca3dc07ca2
javarahul <tripathi.rahul15@gmail.com>
2022-05-10 04:32:54 +0530

    Create NextGreaterToRightElementByStack.java

---
+++ DataStructure/src/stack/NextGreaterToRightElementByStack.java
@@ -0,0 +1,44 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
+
+public class NextGreaterToRightElementByStack {
+
+	public static void main(String[] args) {
+		int arr[] = { 11, 13, 21, 3 };
+		getNextGreaterToRight(arr);
+	}
+
+	public static void getNextGreaterToRight(int[] arr) {
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (stack.size() == 0) {
+				arrayList.add(-1);
+			} else if (stack.size() > 0 && stack.peek() > arr[i]) {
+				arrayList.add(stack.peek());
+			} else if (stack.size() > 0 && stack.peek() < arr[i]) {
+				while (stack.size() > 0 && stack.peek() <= arr[i]) {
+					stack.pop();
+				}
+
+				if (stack.size() == 0) {
+					arrayList.add(-1);
+				} else {
+					arrayList.add(stack.peek());
+				}
+			}
+
+			stack.push(arr[i]);
+		}
+
+		Collections.reverse(arrayList);
+		for (int i = 0; i < arrayList.size(); i++) {
+			System.out.println(arr[i] + ", " + arrayList.get(i));
+		}
+	}
+}
