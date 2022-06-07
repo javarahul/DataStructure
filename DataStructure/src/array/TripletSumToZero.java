04dd24ae3b4355ee880e9d7d9ff278bd10be0f13
javarahul <tripathi.rahul15@gmail.com>
2022-06-07 07:02:57 +0530

    Triplet Sum to Zero
    Given an array of unsorted numbers, find all  triplets in it that add up to zero.

---
+++ DataStructure/src/array/TripletSumToZero.java
@@ -0,0 +1,27 @@
+package array;
+
+public class TripletSumToZero {
+
+	public static void main(String[] args) {
+		int arr[] = { -1, 0, 1, 2, -1, -4 };
+		findTriplets(arr);
+	}
+
+	private static void findTriplets(int arr[]) {
+		boolean found = false;
+		for (int i = 0; i < arr.length - 2; i++) {
+			for (int j = i + 1; j < arr.length - 1; j++) {
+				for (int k = j + 1; k < arr.length; k++) {
+					if (arr[i] + arr[j] + arr[k] == 0) {
+						System.out.println("[" + arr[i] + ", " + arr[j] + ", " + arr[k] + "]");
+						found = true;
+					}
+				}
+			}
+		}
+
+		if (!found) {
+			System.out.println("Triplets not exist");
+		}
+	}
+}
