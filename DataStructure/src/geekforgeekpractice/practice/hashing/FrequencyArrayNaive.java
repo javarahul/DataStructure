732ce9e9a8b40caf0010311dac79f88f5d5b89ff
javarahul <tripathi.rahul15@gmail.com>
2024-03-15 19:17:45 +0530

    Create FrequencyArrayNaive.java

---
+++ DataStructure/src/geekforgeekpractice/practice/hashing/FrequencyArrayNaive.java
@@ -0,0 +1,32 @@
+package geekforgeekpractice.practice.hashing;
+
+public class FrequencyArrayNaive {
+	public static void main(String[] args) {
+		int arr[] = { 15, 12, 13, 12, 13, 13, 18 };
+
+		printFreq(arr);
+	}
+
+	private static void printFreq(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			boolean flag = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					flag = true;
+					break;
+				}
+			}
+
+			if (flag) {
+				continue;
+			}
+			int freq = 1;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					freq++;
+				}
+			}
+			System.out.println(arr[i] + " " + freq);
+		}
+	}
+}
