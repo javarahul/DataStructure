d9b1f2679fbfb5be22309bef561327e6dce6fad1
javarahul <tripathi.rahul15@gmail.com>
2024-03-15 19:16:59 +0530

    Create CountDistNaive.java

---
+++ DataStructure/src/geekforgeekpractice/practice/hashing/CountDistNaive.java
@@ -0,0 +1,32 @@
+package geekforgeekpractice.practice.hashing;
+
+public class CountDistNaive {
+
+	public static void main(String[] args) {
+		int arr[] = { 15, 12, 13, 12, 13, 13, 18 };
+
+		int distElement = countDistinct(arr);
+
+		System.out.println("Total distinct Element:- " + distElement);
+	}
+
+	private static int countDistinct(int arr[]) {
+		int result = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			boolean flag = false;
+
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					flag = true;
+					break;
+				}
+			}
+
+			if (!flag) {
+				result++;
+			}
+		}
+		return result;
+	}
+}
