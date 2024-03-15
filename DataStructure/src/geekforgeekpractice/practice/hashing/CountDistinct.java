100a30ea3ace4ef84f3f0e51a7612f3c24cb7f50
javarahul <tripathi.rahul15@gmail.com>
2024-03-15 19:15:34 +0530

    Create CountDistinct.java

---
+++ DataStructure/src/geekforgeekpractice/practice/hashing/CountDistinct.java
@@ -0,0 +1,23 @@
+package geeksforgeeks.hashing;
+
+import java.util.HashSet;
+
+public class CountDistinct {
+	
+	public static void main(String[] args) {
+		int arr[] = { 15, 12, 13, 12, 13, 13, 18 };
+
+		int distElement = countDistinct(arr);
+
+		System.out.println("Total distinct Element:- " + distElement);
+	}
+
+	private static int countDistinct(int arr[]) {
+		HashSet<Integer> set = new HashSet<Integer>();
+
+		for (int i = 0; i < arr.length; i++) {
+			set.add(arr[i]);
+		}
+		return set.size();
+	}
+}
